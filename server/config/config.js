668e33c87cc23cfa6b9f74369996ba2efccf9eaf
Daniel Campos <danics2000@hotmail.com>
2020-12-14 21:43:41 -0600

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe' 
 } else{
-    urlDB = 'mongodb+srv://cafe-cursoudemy:X1qUAWrB25QmyWSX@cluster1.s2aqf.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
